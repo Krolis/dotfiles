bca55cab784eacb467e6dc07db8cb5eeefac263c
Jakub Stefanski <code@jakubstefanski.com>
2018-02-04 22:54:55 +0100

    Remove X11 related configs

--- install.bash
+++ install.bash
@@ -53,49 +53,11 @@ link_prompt "$DIR/bash/inputrc" "$HOME/.inputrc"
 link_prompt "$DIR/bash/bash_completion" "$HOME/.bash_completion"
 link_prompt "$DIR/bash/completions" "$HOME/.bash_completion.d"
 
-# gnupg
-mkdir -p "$HOME/.gnupg"
-link_prompt "$DIR/gnupg/gpg-agent.conf" "$HOME/.gnupg/gpg-agent.conf"
-
 # windows
 if [[ "$(uname)" == MINGW* ]]; then
     link_prompt "$DIR/win32/mintty/minttyrc" "$HOME/.minttyrc"
-else
-    # tmux
+else # Linux
     link_prompt "$DIR/tmux/tmux.conf" "$HOME/.tmux.conf"
-
-    # X11
-    if ! [ -z "$DISPLAY" ]; then
-        # X
-        link_prompt "$DIR/x11/Xdefaults" "$HOME/.Xdefaults"
-
-        # X Scripts
-        mkdir -p "$HOME/.local/bin"
-        link_prompt "$DIR/x11/scripts/pixlock" "$HOME/.local/bin/pixlock"
-        link_prompt "$DIR/x11/scripts/monitorlayout" "$HOME/.local/bin/monitorlayout"
-        link_prompt "$DIR/x11/scripts/ranger-open" "$HOME/.local/bin/ranger-open"
-
-        # Images
-        mkdir -p "$HOME/Images"
-        link_prompt "$DIR/x11/images/lock.png" "$HOME/Images/lock.png"
-
-        # i3
-        mkdir -p "$HOME/.config/i3"
-        link_prompt "$DIR/x11/i3/config" "$HOME/.config/i3/config"
-        link_prompt "$DIR/x11/i3/i3status.conf" "$HOME/.config/i3/i3status.conf"
-
-        # rofi-pass
-        mkdir -p "$HOME/.config/rofi-pass"
-        link_prompt "$DIR/x11/rofi-pass/config" "$HOME/.config/rofi-pass/config"
-
-        # gtk
-        link_prompt "$DIR/x11/gtk/gtkrc-2.0" "$HOME/.gtkrc-2.0"
-        link_prompt "$DIR/x11/gtk/gtkrc-3.0" "$HOME/.config/gtk-3.0/settings.ini"
-
-        # compton
-        mkdir -p "$HOME/.config/compton"
-        link_prompt "$DIR/x11/compton/compton.conf" "$HOME/.config/compton/compton.conf"
-    fi
 fi
 
 if [ -n "$NEW" ]; then
